8fe2c05e37ad4ff1c7582d4d2da0f7dfb85418ad
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-08 15:39:16 -0800

    Updated schema, added data to tables.

--- bamazon_schema.sql
+++ bamazon_schema.sql
@@ -4,10 +4,34 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
+DROP TABLE IF EXISTS products, departments;
+
 CREATE TABLE products (
   item_id INT AUTO_INCREMENT,
   product_name VARCHAR(80) NOT NULL,
-  department_name VARCHAR(80) NOT NULL,
+  department_id INT NOT NULL,
   price DECIMAL (10, 2) NOT NULL,
-  stock_quantity INT NOT NULL
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
 );
+
+CREATE TABLE departments (
+  department_id INT AUTO_INCREMENT,
+  department VARCHAR(80) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+
+INSERT INTO products (product_name, department_id, price, stock_quantity) VALUES
+("T-Rex Hat", 1, 29.99, 50),
+("Pikachu Hat", 1, 24.99, 25),
+("Totoro Hat", 1, 34.99, 10),
+("Triceratops Shoes", 2, 44.99, 60),
+("Charizard Shoes", 2, 49.99, 35),
+("Ponyo Shoes", 2, 39.99, 20);
+
+INSERT INTO departments (department) VALUES
+("Hats"),
+("Shoes");
+
+SELECT * FROM products;
+SELECT * FROM departments;
