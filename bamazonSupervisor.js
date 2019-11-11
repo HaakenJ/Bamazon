69ddb68f76e969b0be68ecfcf5e7dd5577c7dbc3
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-11 14:39:25 -0800

    Completed the department table function with calculated profit in bamazonSupervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -35,22 +35,25 @@ function listOptions() {
 function displayProducts() {
     connection.query(
         "SELECT " + 
-        "d.department_id, d.department_name, " + 
-        "d.over_head_costs, p.product_sales " + 
-        "FROM departments d " + 
-        "INNER JOIN products p " + 
-        "ON d.department_name = p.department",
+        "d.department_id AS id, p.department AS name, d.over_head_costs AS costs, " + 
+        "SUM(p.product_sales) AS sales, " + 
+        "(p.product_sales - d.over_head_costs) AS profit " + 
+        "FROM products p " + 
+        "INNER JOIN departments d " +
+        "ON d.department_name = p.department " +
+        "GROUP BY p.department",
         (err, res) => {
             if (err) throw err;
             let table = new Table({
-                head: ["Depart. ID", "Dept. Name", "OH Costs", "Total Sales"],
-                colWidths: [10, 30, 10, 10]
+                head: ["Depart. ID", "Dept. Name", "OH Costs", "Total Sales", "Profit / Loss"],
+                colWidths: [15, 15, 15, 15, 15]
             });
             res.forEach(record => {
-                let currentItem = [record.department_id,
-                    record.department_name,
-                    record.over_head_costs,
-                    record.product_sales
+                let currentItem = [record.id,
+                    record.name,
+                    record.costs,
+                    record.sales,
+                    record.profit
                 ];
                 table.push(currentItem);
             })
